c51b4df3597e5768494aabc36921ac80869755ea
siddhesh1010 <siddheshalagade@gmail.com>
2023-01-26 19:45:12 +0530

    render.com

--- server/app.js
+++ server/app.js
@@ -51,7 +51,8 @@ mongoose.connect(MONGOURI,{
 });
 if(process.env.NODE_ENV=="production"){
     app.use(express.static('../client/build'))
-    const path = require('path')
+    const path = require('path');
+    console.log("in production");
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'../clientl','build','index.html'))
     })
