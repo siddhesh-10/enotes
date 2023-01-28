a8dce1b4cf6311daf723e8803ce2cba7a9ba789c
siddhesh1010 <siddheshalagade@gmail.com>
2023-01-28 11:05:13 +0530

    json page fixex

--- server/app.js
+++ server/app.js
@@ -54,7 +54,7 @@ if(process.env.NODE_ENV=="production"){
     const path = require('path');
     console.log("in production");
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'../clientl','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'../client/build/index.html'))
     })
 }
 
